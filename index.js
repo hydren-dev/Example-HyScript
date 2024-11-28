4c156c646c432347bafb6da5b3d8b691cdc70962
Hydren <166227780+hydren-dev@users.noreply.github.com>
2024-11-28 19:59:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const hy = new HyScript();
 // Set the directory containing `.hs` files  
 hy.set('./pages');  
 
+// Public Directory 
+hy.static('public');
+
 // Start the server on a specific port  
 const PORT = 3000;  
 hy.start(PORT, 'HyScript server is running!');  
